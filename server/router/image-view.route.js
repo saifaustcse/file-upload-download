f3250c8d2e70bf32c92e7c2bd4e87b67e4b38880
Saif <saifaustcse26@gmail.com>
2017-06-20 15:46:19 +0600

    typings-update

--- server/router/image-view.route.js
+++ server/router/image-view.route.js
@@ -13,10 +13,14 @@ module.exports = function (app) {
 
         //var str = request.pathname;
         //var action = str.replace(/%20/g, " ");
-     
-        var action = decodeURI(request.pathname);
-        var model = decodeURL(req.params.model) + '/';
-        var name = decodeURI(req.params.name);
+
+        //var action = decodeURI(request.pathname);
+        //var model = decodeURL(req.params.model) + '/';
+        //var name = decodeURI(req.params.name);
+
+        var action = decodeURIComponent(request.pathname);
+        var model = decodeURIComponent(req.params.model) + '/';
+        var name = decodeURIComponent(req.params.name);
 
         if (action == ('/image/' + model + name)) {
             var img = fs.readFileSync(dir + model + name);
